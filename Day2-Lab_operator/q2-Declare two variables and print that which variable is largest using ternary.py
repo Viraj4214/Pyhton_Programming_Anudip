cc069aaeb82c3e35ea64b9b863a69b8a4ad99646
Viraj4214 <85393026+Viraj4214@users.noreply.github.com>
2025-07-16 12:05:09 +0530

    Add files via upload

---
+++ Day2-Lab_operator/q2-Declare two variables and print that which variable is largest using ternary.py
@@ -0,0 +1,9 @@
+#Declare two variables and print that which variable is largest using ternary operators
+
+#declare a variable
+a = 25
+b = 40
+#use ternary operator to find largest variable
+largest = a if a>b else b
+#print the largest variable
+print("The largest variable is ",largest)
